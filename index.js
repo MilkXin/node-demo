9cb7b77ec6479f922ec557523971596742af3ed1
chen xin <729609320@.com>
2019-11-26 22:46:48 +0800

    get&post

---
+++ index.js
@@ -0,0 +1,42 @@
+const http = require('http')
+const querystring = require('querystring')
+
+const server = http.createServer((req, res) => {
+    const method = req.method
+    const url = req.url
+    const path = url.split('?')[0]
+    const query = querystring.parse(url.split('?')[1])
+
+    res.setHeader('Content-type', 'application/json')
+
+    const resData = {
+        method,
+        url,
+        path,
+        query
+    }
+
+    if (method === 'GET') {
+        res.end(
+            JSON.stringify(resData)
+        )
+    }
+
+    if (method === 'POST') {
+        let postData = ''
+        req.on('data', thunk => {
+            postData += thunk.toString()
+        })
+
+        req.on('end', () => {
+            resData.postData = postData
+            res.end(
+                JSON.stringify(resData)
+            )
+        })
+    }
+})
+
+server.listen(3000, () => {
+    console.log('OK')
+})
