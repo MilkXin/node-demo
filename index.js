a8623652402004d0e17c62ce48d5af46dd956caa
chen xin <729609320@.com>
2019-11-30 20:02:40 +0800

    connect mysql

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const http = require('http')
+/* const http = require('http')
 const querystring = require('querystring')
 
 const server = http.createServer((req, res) => {
@@ -39,4 +39,31 @@ const server = http.createServer((req, res) => {
 
 server.listen(3000, () => {
     console.log('OK')
+}) */
+
+
+
+const mysql = require('mysql')
+
+//创建连接对象
+const con = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '123456',
+    port: '3306',
+    database: 'myblog'
+})
+
+//开始连接
+con.connect()
+
+//执行sql语句
+const sql = 'select * from users;'
+con.query(sql, (err, result) => {
+    if (err) {
+        console.log(err)
+        return
+    }
+
+    console.log(result)
 })
