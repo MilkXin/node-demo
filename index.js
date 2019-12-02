472e68b07bd4b040de7df4ee9eb170117953cb00
chen xin <729609320@.com>
2019-12-02 22:53:32 +0800

    文件操作，写入文件

--- index.js
+++ index.js
@@ -99,7 +99,7 @@ const path = require('path')
 const fileName = path.resolve(__dirname, 'data.txt')
 
 //读取文件内容
-fs.readFile(fileName, (err, data) => {
+/* fs.readFile(fileName, (err, data) => {
     if (err) {
         console.error(err)
         return
@@ -107,5 +107,14 @@ fs.readFile(fileName, (err, data) => {
 
     //data是二进制类型，需要转换为字符串
     console.log(data.toString())
+}) */
+
+//写入文件
+const content = '新写入内容\n'
+const opt = {
+    flag: 'a'   //'a'追加写入, 'w'覆盖
+}
+fs.writeFile(fileName, content, opt, (err) => {
+    console.error(err)
 })
 
