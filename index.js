c5ffbdbad767cfa57fe691aef832368ea604e8ca
chen xin <729609320@.com>
2019-12-03 22:40:54 +0800

    stream复制文件

--- index.js
+++ index.js
@@ -126,4 +126,22 @@ fs.writeFile(fileName, content, opt, (err) => {
 
 
 //标准输入输出
-process.stdin.pipe(process.stdout)
+// process.stdin.pipe(process.stdout)
+
+
+
+//stream复制文件
+const fs = require('fs')
+const path = require('path')
+
+const fileName1 = path.resolve(__dirname, 'data.txt')
+const fileName2 = path.resolve(__dirname, 'data-copy.txt')
+
+const readStream = fs.createReadStream(fileName1)
+const writeStream = fs.createWriteStream(fileName2)
+
+readStream.pipe(writeStream)
+
+readStream.on('end', () => {
+    console.log('copy done')
+})
